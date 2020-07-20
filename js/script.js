aba24fd2184b2e202ae662ee9670e41c2bf3506e
Ivan <matteraivam@gmail.com>
2020-07-20 18:05:55 +0200

    aggiunto controllo range numero

--- js/script.js
+++ js/script.js
@@ -18,17 +18,27 @@ var counter= 0;
 // SE IL CONTROLLO è FALSO E NON ABBIAMO SUPERATO GLI 84 TENTATIVI IL CICLO CONTINUERà
 while (numeri.length < 84 && (controllo(arrayPc,utente)) != true) {
   var utente = parseInt(prompt("inserisci un numero da 1 a 100"));
+  check(utente);
   // ORA IMPOSTO I VARI if
+  // SE IL NUMERO INSERITO è PRESENTE NELL ARRAY O LO HA GIà USATO ACCADRà QUESTO
   if (controllo(arrayPc,utente) == true || numeri.includes(utente)) {
     alert ("HAI PERSO punteggio ottenuto: " + counter )
-  }else if (numeri.includes(utente) == false && numeri.length != 84) {
+    // SE IL NUMERO è BUONO SI PROSEGUIRà
+  }else if (numeri.includes(utente) == false && numeri.length != 84 ) {
     numeri.push(utente);
     counter += +1;
+    // SE ARRIVA ALLA FINE HA VINTO
   }else {
-    alert("hai vinto!!");
+    alert("hai vinto");
   }
 }
 
+
+function check (num) {
+  if (  num >100 || num <= 0) {
+    alert(" ATTENZIONE INSERISCI UN NUMERO DA 1 A 100");
+  }
+}
 
 //funzione per numero random
 function getRandom (min, max) {
